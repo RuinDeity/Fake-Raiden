a035ee5393c134b1bc9b7e00600245d7cbe8d963
RuinDeity <t2333333@hotmail.com>
2020-08-10 16:53:22 +0800

    Ruin

---
+++ Assets/sprite/EnemyBullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed;
+    void Update()
+    {
+        Vector2 movement = Vector2.down * speed * Time.deltaTime;
+        transform.Translate(movement); 
+    }
+}
---
+++ Assets/sprite/PlayerBulletSpeed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBulletSpeed : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        Vector2 movement = Vector2.up * speed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+
+}
