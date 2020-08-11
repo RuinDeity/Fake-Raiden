8e3e394aa40e240cf2787136167321d5813f7865
RuinDeity <t2333333@hotmail.com>
2020-08-11 15:36:55 +0800

    12345

--- Assets/Scripts/BulletHit.cs
+++ Assets/Scripts/BulletHit.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class BulletHit : MonoBehaviour
 {
+    private Pause canvas;
+
     public void OnTriggerEnter2D(Collider2D collision)
     {
+
+        if(collision.tag == "Player")
+        {
+            Time.timeScale = 0;
+            canvas = GameObject.FindObjectOfType<Pause>();
+            canvas.turnOnCanvas();
+        }
         Destroy(gameObject);
         Destroy(collision.gameObject);
     }
